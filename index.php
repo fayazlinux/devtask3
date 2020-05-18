3ba8d7eeaf1372dd5c006351558b3d760c928ba3
fayazlinux <kkhome4u@gmail.com>
2020-05-18 12:59:00 +0500

    ppp

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print ( " The Future of Cloud Computing - Hybrid Multi Cloud (AWS | Terraform | OpenStack | Kubernetes) by The World Record Holder Mr. Vimal Daga     !!! ")
+printsss ( " The Future of Cloud Computing - Hybrid Multi Cloud (AWS | Terraform | OpenStack | Kubernetes) by The World Record Holder Mr. Vimal Daga     !!! ")
 ?>
