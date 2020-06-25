2d080f01e03ead3a142874d7037c2a749b7a03ec
fayazlinux <64469235+fayazlinux@users.noreply.github.com>
2020-06-26 01:03:45 +0500

    new comment22

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print ( " LINUX  FOR EVERYONE  The Future of Cloud Computing - Hybrid Multi Cloud (AWS | Terraform | OpenStack | Kubernetes) by The World Record Holder Mr. Vimal Daga     !!! ")
+printsss ( " LINUX  FOR EVERYONE  The Future of Cloud Computing - Hybrid Multi Cloud (AWS | Terraform | OpenStack | Kubernetes) by The World Record Holder Mr. Vimal Daga     !!! ")
 ?>
